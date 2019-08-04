c1bc49e2387515ff8c55aa92a864a7f43c3451ca
Yusuke Kuoka <ykuoka@gmail.com>
2019-08-04 20:15:07 +0900

    fix

--- http.js
+++ http.js
@@ -20,13 +20,12 @@ const request = function (urlstr, options) {
         options.headers['User-Agent'] = 'Brigade-Worker'
         params = options.parameters
         delete options.parameters
-        let body
+        console.log('http.request.options', options)
         if (params) {
             body = JSON.stringify(params)
             options.headers['Content-Length'] = body.length
+            console.log('http.request.body', body)
         }
-        console.log('http.request.options', options)
-        console.log('http.request.body', body)
         const request = lib.request(options, (response) => {
             console.log('http.response', { status: `${response.statusCode}`, headers: JSON.stringify(response.headers) });
             response.setEncoding('utf8');
